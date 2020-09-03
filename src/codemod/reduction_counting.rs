5a02ce5f82a7c72789c8dee689f78abeb5133b63
Bernard Kolobara <me@kolobara.com>
2020-09-03 15:20:57 +0200

    Fix func index offsets after injecting reduction counting

--- src/codemod/reduction_counting.rs
+++ src/codemod/reduction_counting.rs
@@ -1,25 +1,59 @@
-use  parity_wasm::elements::{serialize, deserialize_buffer, Module, Instruction, BlockType};
+use  parity_wasm::elements::{serialize, deserialize_buffer, Module, Instruction, BlockType, External, Internal};
 use parity_wasm::builder::{from_module, signature, import, ModuleBuilder};
 
+// How many function calls should happen before we yield
+const REDUCTION_LIMIT: i32 = 100_000;
+
+/// Modifies the WASM binary to add a `yield` function call after `REDUCTION_LIMIT` of **operations** is reached.
+/// Currently only function calls are counted as **operations**.
+/// TODO: If there is a tight loop without function calls, this becomes an issue. We need to check for this case.
+/// The idea behind this is to not allow any WASM Instance to block a thread in an async environment for too long.
+/// To achive this the following things are inserted into the WASM module:
+/// * Global variable to hold the current count
+/// * An import to the host provided `yield` function
+/// * Instructions on top of each function to check if we reached the `REDUCTION_LIMIT` and yield
+///
+/// Some of the insertions can break the existing code. E.g. if we insert a new imported function, all the indexes
+/// in **functino calls** and **exports** become invalid and need to be updated.
 pub fn insert_reduction_counting(wasm_buff: &[u8]) -> Vec<u8> {
     let wasm: Module = deserialize_buffer(wasm_buff).unwrap();
     // Determine the index of the to be inserted global variable.
+    // This was fixed in: https://github.com/paritytech/parity-wasm/pull/291
+    // Once the release is out, update.
     let reduction_counter = if let Some(global_section) = wasm.global_section() {
         global_section.entries().len() as u32
     } else {
         0
     };
+
+    // Determin how many function imports there are before we inject a new one so we can offset
+    // all function calls & exports by the correct value.
+    let yield_import = if let Some(import_section) = &wasm.import_section() {
+        import_section.entries().iter().fold(0, |acc, import|
+            match import.external() {
+                External::Function(_) => acc + 1,
+                _ => acc
+            }
+        )
+    } else {
+        0
+    };
+
     let builder = from_module(wasm);
     let builder = inject_global_reduction_counter(builder);
-    let (yield_import, builder) = inject_yield_import(builder);
-    let module = add_reduction_counter_to_all_functions(
+
+    let builder = inject_yield_import(builder);
+    let module = inject_reduction_counter_to_all_functions(
         builder.build(),
         reduction_counter,
         yield_import
     );
+
+    let module = update_export_indexes(module, yield_import);
     serialize(module).unwrap()
 }
 
+/// Injects a global mutable value initalised with 0.
 fn inject_global_reduction_counter(builder: ModuleBuilder) -> ModuleBuilder {
     builder.global()
         .value_type().i32()
@@ -28,35 +62,76 @@ fn inject_global_reduction_counter(builder: ModuleBuilder) -> ModuleBuilder {
         .build()
 }
 
-fn inject_yield_import(mut builder: ModuleBuilder) -> (u32, ModuleBuilder) {
+/// Injects a function import.
+fn inject_yield_import(mut builder: ModuleBuilder) -> ModuleBuilder {
     let yield_sig = builder.push_signature(
 		signature().build_sig()
     );
-    let yield_import = builder.push_import(
+    builder.push_import(
         import()
         .module("lunatic")
-		.field("yielder")
+		.field("yield")
 		.external().func(yield_sig)
 		.build()
     );
-    (yield_import, builder)
+    builder
 }
 
-fn add_reduction_counter_to_all_functions(mut module: Module, reduction_counter: u32, yielder: u32) -> Module {
-    let mut check_if_reduction_count_reached_10k_instructions = Vec::with_capacity(10);
-    check_if_reduction_count_reached_10k_instructions.push(Instruction::GetGlobal(reduction_counter));
-    check_if_reduction_count_reached_10k_instructions.push(Instruction::I32Const(1));
-    check_if_reduction_count_reached_10k_instructions.push(Instruction::I32Add);
-    check_if_reduction_count_reached_10k_instructions.push(Instruction::I32Const(10_000));
-    check_if_reduction_count_reached_10k_instructions.push(Instruction::I32GtU);
-    check_if_reduction_count_reached_10k_instructions.push(Instruction::If(BlockType::NoResult));
-    check_if_reduction_count_reached_10k_instructions.push(Instruction::Call(yielder));
-    check_if_reduction_count_reached_10k_instructions.push(Instruction::End);
+/// Inject code for reduction counting on top of each funciton and update function calls to the right offset.
+fn inject_reduction_counter_to_all_functions(mut module: Module, reduction_counter: u32, yielder: u32) -> Module {
+    // Algorithm:
+    // 1. Increment the reduction counter global
+    // 2. Check if the global reached 10_000, if yes yiled and reset reduction counter
+    let mut injected_instructions = Vec::with_capacity(12);
+    injected_instructions.push(Instruction::GetGlobal(reduction_counter));
+    injected_instructions.push(Instruction::I32Const(1));
+    injected_instructions.push(Instruction::I32Add);
+    injected_instructions.push(Instruction::SetGlobal(reduction_counter));
+    injected_instructions.push(Instruction::GetGlobal(reduction_counter));
+    injected_instructions.push(Instruction::I32Const(REDUCTION_LIMIT));
+    injected_instructions.push(Instruction::I32GtS);
+    injected_instructions.push(Instruction::If(BlockType::NoResult));
+    injected_instructions.push(Instruction::Call(yielder));
+    injected_instructions.push(Instruction::I32Const(0));
+    injected_instructions.push(Instruction::SetGlobal(reduction_counter));
+    injected_instructions.push(Instruction::End);
 
     if let Some(code_section) = module.code_section_mut() {
         for func in code_section.bodies_mut() {
-            let instructions = func.code_mut().elements_mut();
-            instructions.splice(0..0, check_if_reduction_count_reached_10k_instructions.clone());
+            let instructions = func.code().elements();
+            // Offset all call instructinos by 1 if they point to an index after the yielder.
+            let mut instructions: Vec<Instruction> = instructions.iter().map(|instruction| match instruction {
+                Instruction::Call(index) =>
+                    if *index >= yielder {
+                        Instruction::Call(index + 1)
+                    } else {
+                        instruction.clone()
+                    },
+                _ => instruction.clone()
+            }).collect();
+
+            // Add a reduction counter check before any other code in the function.
+            // We need to clone every time, to not polute injected_instructions for the next function.
+            let mut new_function = injected_instructions.clone();
+            new_function.append(&mut instructions);
+
+            *func.code_mut().elements_mut() = new_function;
+        }
+    }
+    module
+}
+
+/// Updated exported functions with the right offset, becuase we added one more imported function.
+fn update_export_indexes(mut module: Module, yielder: u32) -> Module {
+    if let Some(export_section) = module.export_section_mut() {
+        for export in export_section.entries_mut() {
+            match export.internal() {
+                Internal::Function(index) =>
+                    if *index >= yielder {
+                        *export.internal_mut() = Internal::Function(*index + 1);
+                    },
+                _ => ()
+            }
         }
     }
     module
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,58 @@
 use async_wormhole::{AsyncWormhole, AsyncYielder};
 use anyhow::Result;
 use wasmtime::*;
+
 use std::mem::ManuallyDrop;
+use std::fs::File;
+use std::io::prelude::*;
 
-use smol::Timer;
-use std::time::{Duration, Instant};
 
-use tokio::prelude::*;
+
 use tokio::runtime::Runtime;
-use tokio::time::delay_for;
+use tokio::task::yield_now;
+
+use lunatic::codemod::reduction_counting::insert_reduction_counting;
 
 fn main() -> Result<()> {
     // All wasm objects operate within the context of a "store"
     let store = Store::default();
 
     // Modules can be compiled through either the text or binary format
-    let wat = r#"
-        (module
-            (import "" "" (func $minus_42 (param i32)))
+    let test = include_bytes!("test_yield.wasm");
+    let test = insert_reduction_counting(test);
+    
+    let mut file = File::create("dump.wasm")?;
+    file.write_all(&test)?;
 
-            (func (export "hello")
-                i32.const 45
-                call $minus_42)
-        )
-    "#;
-    let module = Module::new(store.engine(), wat)?;
+    let module = Module::new(store.engine(), test)?;
 
     let mut wasm = AsyncWormhole::new(move |yielder| {
         
         let yielder_ptr = &yielder as *const AsyncYielder<()> as usize;
 
-        let minus_42 = Func::wrap(&store, move |_param: i32| {
+        let yield_ = Func::wrap(&store, move || {
             let mut yielder = unsafe {
                 std::ptr::read(yielder_ptr as *const ManuallyDrop<AsyncYielder<()>>)
             };
-            println!("Now wait");
-            let now = Instant::now();
-            yielder.async_suspend( delay_for(Duration::from_secs(5)) );
-            println!("{}", now.elapsed().as_nanos());
+            
+            println!("Yielded");
+            yielder.async_suspend( yield_now() );
         });
 
         
         // Instantiation of a module requires specifying its imports and then
         // afterwards we can fetch exports by name, as well as asserting the
         // type signature of the function with `get0`.
-        let instance = Instance::new(&store, &module, &[minus_42.into()]).unwrap();
+        let instance = Instance::new(&store, &module, &[yield_.into()]).unwrap();
         let hello = instance
             .get_func("hello")
             .ok_or(anyhow::format_err!("failed to find `hello`  function export")).unwrap()
             .get0::<()>().unwrap();
 
         // And finally we can call the wasm as if it were a Rust function!
-        hello().unwrap();
+        let now = std::time::Instant::now();
+        hello();
+        println!("{}", now.elapsed().as_millis());
     })?;
 
     wasm.preserve_tls(&wasmtime_runtime::traphandlers::tls::PTR);
