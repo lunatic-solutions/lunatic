26e58771b4bba02c2e3d39abb08b6857da00fbf1
Bernard Kolobara <me@kolobara.com>
2021-01-26 14:29:04 +0100

    Use clap for command line arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(available_concurrency)]
-
 pub mod channel;
 pub mod linker;
 pub mod module;
@@ -7,46 +5,3 @@ pub mod networking;
 pub mod normalisation;
 pub mod process;
 pub mod wasi;
-
-use anyhow::Result;
-use easy_parallel::Parallel;
-
-use process::{FunctionLookup, MemoryChoice, Process, EXECUTOR};
-
-use std::env;
-use std::fs;
-use std::thread;
-
-pub fn run() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let wasm_path = args.get(1).expect("Not enough arguments passed");
-    let wasm = fs::read(wasm_path).expect("Can't open WASM file");
-
-    let module = module::LunaticModule::new(wasm)?;
-
-    // Set up async runtime
-    let cpus = thread::available_concurrency().unwrap();
-    let (signal, shutdown) = smol::channel::unbounded::<()>();
-
-    Parallel::new()
-        .each(0..cpus.into(), |_| {
-            smol::future::block_on(EXECUTOR.run(shutdown.recv()))
-        })
-        .finish(|| {
-            smol::future::block_on(async {
-                let result = Process::spawn(
-                    None,
-                    module,
-                    FunctionLookup::Name("_start"),
-                    MemoryChoice::New,
-                )
-                .join()
-                .await;
-                drop(signal);
-                result
-            })
-        })
-        .1?;
-
-    Ok(())
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,57 @@
+#![feature(available_concurrency)]
+
 use anyhow::Result;
+use easy_parallel::Parallel;
+
+use clap::{crate_version, Clap};
+use lunatic_runtime::module;
+use lunatic_runtime::process::{FunctionLookup, MemoryChoice, Process, EXECUTOR};
+
+use std::fs;
+use std::thread;
+
+#[derive(Clap)]
+#[clap(version = crate_version!())]
+struct Opts {
+    /// .wasm file
+    input: String,
+}
+
+pub fn run() -> Result<()> {
+    let opts: Opts = Opts::parse();
+
+    let wasm = fs::read(opts.input).expect("Can't open .wasm file");
+
+    let module = module::LunaticModule::new(wasm)?;
+
+    // Set up async runtime
+    let cpus = thread::available_concurrency().unwrap();
+    let (signal, shutdown) = smol::channel::unbounded::<()>();
+
+    Parallel::new()
+        .each(0..cpus.into(), |_| {
+            smol::future::block_on(EXECUTOR.run(shutdown.recv()))
+        })
+        .finish(|| {
+            smol::future::block_on(async {
+                let result = Process::spawn(
+                    None,
+                    module,
+                    FunctionLookup::Name("_start"),
+                    MemoryChoice::New,
+                )
+                .join()
+                .await;
+                drop(signal);
+                result
+            })
+        })
+        .1?;
+
+    Ok(())
+}
 
 fn main() -> Result<()> {
     env_logger::init();
-    lunatic_runtime::run()
+    run()
 }
