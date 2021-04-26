8cd2f67713606f33e6d27086f5dc7b9d178b1625
Bernard Kolobara <me@kolobara.com>
2021-04-26 16:17:48 +0200

    Add host call overhead benchmarks

---
+++ benches/guest/rust/src/host_calls.rs
@@ -0,0 +1,15 @@
+use easybench::bench;
+use lunatic::yield_;
+
+mod host {
+    #[link(wasm_import_module = "wasi_snapshot_preview1")]
+    extern "C" {
+        pub fn proc_raise(_signal: u32) -> u32;
+    }
+}
+
+pub fn bench_host_calls() {
+    println!("Call yield_: {}", bench(|| yield_() ));
+    // no-op
+    println!("Call wasi_snapshot_preview1::proc_raise: {}", bench(|| unsafe {host::proc_raise(0)} ));
+}
--- benches/guest/rust/src/main.rs
+++ benches/guest/rust/src/main.rs
@@ -1,6 +1,9 @@
 mod channel;
+mod host_calls;
 
 fn main() {
-    println!("-- Channel --");
+    println!("\n-- Channel --");
     channel::bench_channel();
+    println!("\n-- Host calls --");
+    host_calls::bench_host_calls();
 }
