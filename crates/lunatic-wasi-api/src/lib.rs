3e0e83f92d674f9a0eb34c86a91888588b0732d3
Hrvoje Bandov <hrvoje.bandov@gmail.com>
2022-02-16 09:35:52 +0100

    Move WASI host fns into a separate crate

--- crates/lunatic-wasi-api/src/lib.rs
+++ crates/lunatic-wasi-api/src/lib.rs
@@ -1,18 +1,40 @@
 use anyhow::Result;
 use lunatic_common_api::namespace_matches_filter;
 use wasmtime::{Linker, Trap};
+use wasmtime_wasi::{ambient_authority, Dir, WasiCtx, WasiCtxBuilder};
 
-use crate::state::ProcessState;
+pub fn build_wasi(
+    args: Option<&Vec<String>>,
+    envs: Option<&Vec<(String, String)>>,
+    dirs: &[String],
+) -> Result<WasiCtx> {
+    let mut wasi = WasiCtxBuilder::new().inherit_stdio();
+    if let Some(envs) = envs {
+        wasi = wasi.envs(envs)?;
+    }
+    if let Some(args) = args {
+        wasi = wasi.args(args)?;
+    }
+    for preopen_dir_path in dirs {
+        let preopen_dir = Dir::open_ambient_dir(preopen_dir_path, ambient_authority())?;
+        wasi = wasi.preopened_dir(preopen_dir, preopen_dir_path)?;
+    }
+    Ok(wasi.build())
+}
+
+pub trait LunaticWasiCtx {
+    fn wasi(&mut self) -> &mut WasiCtx;
+}
 
 // Register WASI APIs to the linker
-pub(crate) fn register(
-    linker: &mut Linker<ProcessState>,
+pub fn register<T: LunaticWasiCtx + Send + 'static>(
+    linker: &mut Linker<T>,
     namespace_filter: &[String],
 ) -> Result<()> {
     // Add all WASI functions at first
     wasmtime_wasi::sync::snapshots::preview_1::add_wasi_snapshot_preview1_to_linker(
         linker,
-        |ctx| &mut ctx.wasi,
+        |ctx| ctx.wasi(),
     )?;
 
     // Override all functions not matched with a trap implementation.
