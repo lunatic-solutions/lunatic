4b0c97f4f1966b665a7e8bba4f3d6d7fc715cfcb
Bernard Kolobara <me@kolobara.com>
2022-03-30 12:55:19 +0200

    Add filtering out of tests

--- src/mode/cargo_test.rs
+++ src/mode/cargo_test.rs
@@ -23,6 +23,12 @@ pub(crate) async fn test() -> Result<()> {
                 .required(true),
         )
         .arg(
+            Arg::new("filter")
+                .value_name("FILTER")
+                .help("Run only tests that contain the filter string")
+                .required(false),
+        )
+        .arg(
             Arg::new("dir")
                 .long("dir")
                 .value_name("DIRECTORY")
@@ -31,9 +37,22 @@ pub(crate) async fn test() -> Result<()> {
                 .takes_value(true),
         )
         .arg(
+            Arg::new("ignored")
+                .long("ignored")
+                .help("Run only ignored tests")
+                .required(false),
+        )
+        .arg(
             Arg::new("nocapture")
                 .long("nocapture")
                 .help("Don't hide output from test executions")
+                .required(false)
+                .takes_value(false),
+        )
+        .arg(
+            Arg::new("showoutput")
+                .long("show-output")
+                .help("Show also the output of successfull tests")
                 .required(false)
                 .takes_value(false),
         )
@@ -81,31 +100,60 @@ pub(crate) async fn test() -> Result<()> {
     let module = fs::read(path)?;
     let module = runtime.compile_module::<DefaultProcessState>(module)?;
 
+    let filter = args.value_of("filter").unwrap_or_default();
+
     // Find all function exports starting with `#lunatic_test_`.
-    // Functions with a name that matches `#lunatic_test_#trap_Panic message#` are expected to
+    // Functions with a name that matches `#lunatic_test_#panic_Panic message#` are expected to
     // trap with a message that contains "Panic message".
     let mut test_functions = Vec::new();
     for export in module.exports() {
         if let wasmtime::ExternType::Func(_) = export.ty() {
             let wasm_export_name = export.name();
             if wasm_export_name.starts_with("#lunatic_test_") {
-                let name = wasm_export_name.strip_prefix("#lunatic_test_").unwrap();
+                let mut name = wasm_export_name.strip_prefix("#lunatic_test_").unwrap();
+                let mut ignored = false;
+                if name.starts_with("#ignore_") {
+                    name = name.strip_prefix("#ignore_").unwrap();
+                    ignored = true;
+                }
+                // If --ignored flag is present, don't ignore test & filter out non-ignored ones
+                if args.is_present("ignored") {
+                    if ignored {
+                        ignored = false
+                    } else {
+                        // Filter out not ignored tests. The name doesn't need to be preserved,
+                        // because filtered out tests don't show up in the output.
+                        test_functions.push(Test {
+                            filtered: true,
+                            wasm_export_name: "".to_string(),
+                            function_name: "".to_string(),
+                            panic: None,
+                            ignored: false,
+                        });
+                        continue;
+                    }
+                }
                 // Check if test should panic
                 let test = if name.starts_with("#panic_") {
                     let name = name.strip_prefix("#panic_").unwrap();
                     // TODO: Handle escaped `\#`
                     let panic: String = name.chars().take_while(|c| c.ne(&'#')).collect();
                     let panic_prefix = format!("{}#", panic);
+                    let function_name = name.strip_prefix(&panic_prefix).unwrap().to_string();
                     Test {
+                        filtered: !function_name.contains(filter),
                         wasm_export_name: wasm_export_name.to_string(),
-                        function_name: name.strip_prefix(&panic_prefix).unwrap().to_string(),
+                        function_name,
                         panic: Some(panic),
+                        ignored,
                     }
                 } else {
                     Test {
+                        filtered: !name.contains(filter),
                         wasm_export_name: wasm_export_name.to_string(),
                         function_name: name.to_string(),
                         panic: None,
+                        ignored,
                     }
                 };
                 test_functions.push(test);
@@ -113,7 +161,8 @@ pub(crate) async fn test() -> Result<()> {
         }
     }
 
-    let n = test_functions.len();
+    let n = test_functions.iter().filter(|test| !test.filtered).count();
+    let filtered_out = test_functions.len() - n;
     println!("\nrunning {} {}", n, if n == 1 { "test" } else { "tests" });
 
     let (sender, receiver) = async_std::channel::unbounded();
@@ -121,6 +170,24 @@ pub(crate) async fn test() -> Result<()> {
     let config = Arc::new(config);
 
     for test_function in test_functions {
+        // Skip over filtered out functions
+        if test_function.filtered {
+            continue;
+        }
+
+        // Skip over ignored tests
+        if test_function.ignored {
+            sender
+                .send(TestResult {
+                    name: test_function.function_name,
+                    status: TestStatus::Ignored,
+                    stdout: String::new(),
+                })
+                .await
+                .unwrap();
+            continue;
+        }
+
         let mut state =
             DefaultProcessState::new(runtime.clone(), module.clone(), config.clone()).unwrap();
         // Use in-memory stdout & stderr to hide output in case of success and no `--nocapture` flag.
@@ -236,9 +303,8 @@ pub(crate) async fn test() -> Result<()> {
         });
     }
 
-    let mut passed = 0;
-    let ignored = 0;
-
+    let mut ignored = 0;
+    let mut successes = Vec::new();
     let mut failures = Vec::new();
 
     // Wait for all tests to finish
@@ -247,32 +313,70 @@ pub(crate) async fn test() -> Result<()> {
         let name = result.name;
         match result.status {
             TestStatus::Ok => {
+                // If --nocapture is present, print all output
+                if args.is_present("nocapture") {
+                    print!("{}", result.stdout);
+                }
                 println!("test {} ... \x1b[92mok\x1b[0m", name); // green ok
-                passed += 1;
+                successes.push((name, result.stdout));
             }
             TestStatus::Failed => {
+                // If --nocapture is present, print all output
+                if args.is_present("nocapture") {
+                    print!("{}", result.stdout);
+                }
                 println!("test {} ... \x1b[91mFAILED\x1b[0m", name); // red FAIL
                 failures.push((name, result.stdout));
             }
             TestStatus::PanicOk => {
+                // If --nocapture is present, print all output
+                if args.is_present("nocapture") {
+                    print!("{}", result.stdout);
+                }
                 println!("test {} - should panic ... \x1b[92mok\x1b[0m", name); // green ok
-                passed += 1;
+                successes.push((name, result.stdout));
             }
             TestStatus::PanicFailed => {
+                // If --nocapture is present, print all output
+                if args.is_present("nocapture") {
+                    print!("{}", result.stdout);
+                }
                 println!("test {} - should panic ... \x1b[91mFAILED\x1b[0m", name); // red FAIL
                 failures.push((name, result.stdout));
+            }
+            TestStatus::Ignored => {
+                println!("test {} ... \x1b[93mignored\x1b[0m", name); // yellow ignored
+                ignored += 1;
             }
         }
     }
 
-    if !failures.is_empty() {
-        println!("\nfailures:\n");
+    // If --show-output is present, print success outputs
+    if args.is_present("showoutput") {
+        println!("\nsuccesses:");
+        if !args.is_present("nocapture") {
+            // Print stdout of failures
+            for (success, stdout) in successes.iter() {
+                println!("\n---- {} stdout ----", success);
+                print!("{}", stdout);
+            }
+        }
+        println!("\nsuccesses:");
+        for (success, _) in successes.iter() {
+            println!("    {}", success);
+        }
     }
 
-    // Print stdout of failures
-    for (failure, stdout) in failures.iter() {
-        println!("---- {} stdout ----", failure);
-        println!("{}", stdout);
+    if !failures.is_empty() {
+        println!("\nfailures:");
+    }
+
+    if !args.is_present("nocapture") {
+        // Print stdout of failures
+        for (failure, stdout) in failures.iter() {
+            println!("\n---- {} stdout ----", failure);
+            print!("{}", stdout);
+        }
     }
 
     // List failures
@@ -291,8 +395,8 @@ pub(crate) async fn test() -> Result<()> {
         "\x1b[91mFAILED\x1b[0m"
     };
     println!(
-        "\ntest result: {}. {} passed; {} failed; {} ignored; 0 measured; 0 filtered out; finished in {:.2}s\n",
-        result, passed, failures.len(), ignored, now.elapsed().as_millis() as f64 / 1000f64
+        "\ntest result: {}. {} passed; {} failed; {} ignored; 0 measured; {} filtered out; finished in {:.2}s\n",
+        result, successes.len(), failures.len(), ignored, filtered_out, now.elapsed().as_millis() as f64 / 1000f64
     );
 
     if failures.is_empty() {
@@ -308,6 +412,8 @@ struct Test {
     wasm_export_name: String,
     function_name: String,
     panic: Option<String>,
+    filtered: bool,
+    ignored: bool,
 }
 
 #[derive(Debug)]
@@ -323,4 +429,5 @@ enum TestStatus {
     Failed,
     PanicOk,
     PanicFailed,
+    Ignored,
 }
