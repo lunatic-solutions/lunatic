9d0e26f8a86f21cfaae744b0a035a1d768c3165d
Marin Koštić <kosticmarin@gmail.com>
2022-07-27 15:50:47 +0200

    Add tcp implementation as feature flag

--- crates/lunatic-distributed/src/quic/mod.rs
+++ crates/lunatic-distributed/src/quic/mod.rs
@@ -2,9 +2,14 @@
 mod quin;
 #[cfg(feature = "quic-s2n")]
 mod s2n;
+#[cfg(feature = "tcp")]
+mod tcp;
 
 #[cfg(feature = "quic-quinn")]
 pub use quin::*;
 
 #[cfg(feature = "quic-s2n")]
 pub use s2n::*;
+
+#[cfg(feature = "tcp")]
+pub use tcp::*;
