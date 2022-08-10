dbd08fbeef37b7fa54b45624904236556d870268
Marin Koštić <kosticmarin@gmail.com>
2022-08-10 11:09:38 +0200

    Refresh nodes after node registration to include itself

--- crates/lunatic-distributed/src/control/client.rs
+++ crates/lunatic-distributed/src/control/client.rs
@@ -56,11 +56,12 @@ impl Client {
         };
         // Spawn reader task before register
         tokio::task::spawn(reader_task(client.clone()));
-        tokio::task::spawn(refresh_nodes_task(client.clone()));
         let Registered {
             node_id,
             signed_cert,
         } = client.send_registration(signing_request).await?;
+        // Refresh nodes after registration
+        tokio::task::spawn(refresh_nodes_task(client.clone()));
 
         Ok((node_id, client, signed_cert))
     }
