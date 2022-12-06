529625d4bf0c4105cb40bdfe84deed5818f1be56
Stjepan Teskera <stjepan.teskera@gmail.com>
2022-12-06 16:34:23 +0100

    Add env_id to get_module

--- crates/lunatic-distributed/src/control/client.rs
+++ crates/lunatic-distributed/src/control/client.rs
@@ -227,14 +227,15 @@ impl Client {
         self.inner.node_ids.read().unwrap().len()
     }
 
-    pub async fn get_module(&self, module_id: u64) -> Result<Vec<u8>> {
+    pub async fn get_module(&self, module_id: u64, environment_id: u64) -> Result<Vec<u8>> {
         log::info!("Get module {module_id}");
         let url = self
             .inner
             .reg
             .urls
             .get_module
-            .replace("{id}", &module_id.to_string());
+            .replace("{id}", &module_id.to_string())
+            .replace("{env}", &environment_id.to_string());
         let resp: ModuleBytes = self.get(&url, None).await?;
         Ok(resp.bytes)
     }
